b71d21cf4c4b8f866c4f216c56e1fac29f82a247
schen26-debug <schen26@risd.edu>
2026-01-22 10:46:46 -0500

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,11 +1,11 @@
 const textDisplay = document.getElementById('overlay');
 
-// Initialize SpeechRecognition
+
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
-recognition.lang = 'en-US'; // English (United States)
-// recognition.lang = 'ko-KR'; // Korean (Korea)
+recognition.lang = 'en-US';
+
 
 recognition.interimResults = true;
 recognition.continuous = true;
@@ -14,9 +14,9 @@ let silenceTimer;
 
 textDisplay.textContent = '';
 
-// Display recognized text
+
 recognition.onresult = function(event) {
-    //const transcript = event.results[event.results.length - 1][0].transcript;
+
     textDisplay.textContent = 'be quiet';
     textDisplay.classList.add('active');
 
@@ -29,12 +29,12 @@ recognition.onresult = function(event) {
 };
 
 
-// Error handling
+
 recognition.onerror = function(event) {
     console.error('Speech recognition error:', event.error);
 };
 
-// Start speech recognition
+
 recognition.start();
 
 
